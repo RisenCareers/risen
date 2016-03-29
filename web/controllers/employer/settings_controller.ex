79f2fa9cb843b3c5f56de25965836c9b6e302aae
VICKY LEONG <VLEONG2332@GMAIL.COM>
2016-03-28 21:33:49 -0400

    Add content and style to empl/settings page
    The majority of the code and style is copied from empl/setup/edit since
    it's similar. Maybe it can be extracted as a partial and used in both
    empl/settings/show and empl/setup/edit.
    
    Remove unneeded code from students_view.
    
    Link employer_app_menu up to the settings page.

--- web/controllers/employer/settings_controller.ex
+++ web/controllers/employer/settings_controller.ex
@@ -1,13 +1,76 @@
 defmodule Risen.Employer.SettingsController do
   use Risen.Web, :controller
 
-  plug :put_layout, "employer.html"
+  import Risen.Plugs.Authenticator
+  import Risen.Plugs.Employer.Authenticator
 
-  def show(conn, _params) do
-    render conn, "show.html"
+  alias Risen.Employer
+  alias Risen.EmployerLogo
+  alias Risen.EmployerMajor
+  alias Risen.Major
+
+  plug :put_layout, "employer.html"
+  plug :authenticate
+  plug :require_employer
+  plug :require_employer_admin
+  plug :scrub_params, "employer" when action in [:update]
+
+  def show(conn, params) do
+
+    # Retrieve employer via the Authenticator plug
+    employer = conn.assigns[:employer]
+
+    # Preload the majors
+    employer = Repo.preload(employer, [:majors])
+
+    majors = Repo.all(from m in Major)
+    changeset = Employer.changeset(employer)
+
+    conn
+    |> assign(:employer, employer)
+    |> assign(:majors, majors)
+    |> assign(:changeset, changeset)
+    |> render("show.html")
+
   end
 
-  def update(conn, _params) do
-    render conn, "show.html"
+  def update(conn, params) do
+
+    # We got our employer from the Employer authenticator plug
+    employer = conn.assigns[:employer]
+
+    # Preload the majors
+    employer = Repo.preload(employer, [:majors])
+
+    # Update the employer logo
+    if params["logo"] do
+      EmployerLogo.store({params["logo"], employer})
+      employer_changeset = Ecto.Changeset.change(employer, logo: params["logo"].filename)
+      employer = Repo.update!(employer_changeset)
+    end
+
+    # Remove all current employer majors
+    Ecto.Query.from(
+      em in EmployerMajor,
+      where: em.employer_id == ^employer.id
+    )
+    |> Repo.delete_all
+
+    # Update the employer interests
+    if params["employer"]["majors"] do
+      Repo.transaction fn ->
+        Enum.each(params["employer"]["majors"], fn m ->
+          {m_id, _} = Integer.parse(m)
+          Repo.insert!(%EmployerMajor{
+            major_id: m_id,
+            employer_id: employer.id
+          })
+        end)
+      end
+    end
+
+    conn
+    |> redirect(to: employer_students_path(conn, :index, employer.slug))
+    |> halt()
   end
 end
