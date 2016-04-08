ebb4cbad0f7a848c873138ba588d3afc10bcd13b
Stephen Watkins <stjowa@gmail.com>
2016-04-07 22:38:28 -0400

    Update some css

--- web/controllers/employer/batches_controller.ex
+++
@@ -1,9 +0,0 @@
-defmodule Risen.Employer.BatchesController do
-  use Risen.Web, :controller
-
-  plug :put_layout, "employer.html"
-
-  def show(conn, _params) do
-    render conn, "show.html"
-  end
-end
